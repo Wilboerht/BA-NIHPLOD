8e9b9b596501cd040211e829ab4eee9c5515a89b
Hank <hongkunw2002@gmail.com>
2026-05-19 16:45:39 +0800

    1

--- components/certificate/CertificateGenerator.tsx
+++ components/certificate/CertificateGenerator.tsx
@@ -254,7 +254,7 @@ export default function CertificateGenerator({ initialData, mode = 'create', isV
       });
     }
 
-    currentY += 22 * scale;
+    currentY += 20 * scale;
 
     // 授权业务范围
     offCtx.font = `400 ${15 * scale}px "Noto Serif SC", serif`;
